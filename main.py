c0a7744c304dc4a706f89bdb1331e073937dbe5f
artemis-beta <KXZ186@student.bham.ac.uk>
2016-04-13 22:45:20 +0100

    Created new module for weather

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import telepot
 import time
 from telepot.delegate import per_chat_id, create_open
+import weather_bot
 
-##########OPENWEATHER API & APPKEY##################
-import pyowm
-owm = pyowm.OWM('238da21e83fcfa9f499bbad8d9b365d0')
 ####################################################
 
 class WestieBot(telepot.helper.ChatHandler):
@@ -18,22 +16,8 @@ class WestieBot(telepot.helper.ChatHandler):
 	
 	#OpenWeather Implementation, check for 'weather' in input string
 	if 'weather' in msg['text']:
-	    weatherLocationW313 = owm.weather_at_place('Birmingham,GB')
-	    w = weatherLocationW313.get_weather()
-	    weather = {'Status': w.get_detailed_status(), 
-		       'Time':w.get_reference_time(timeformat='iso'),
-		       'Humidity': w.get_humidity(),
-                       'Pressure': w.get_pressure(),
-                       'Temperature' : w.get_temperature(unit='celsius'),
-                       'Wind' : w.get_wind()}
-			
-	    self.sender.sendMessage(""" Weather at West 313 is currently: 
-					\nDate/Time: %s \nStatus: %s \nTemperature: %s C, High: %s C, Low: %s C 
-					\nHumidity: %s \nPressure: %s mb\nWind Speed: %s mph, %s Degrees"""
-			           % (weather['Time'],weather['Status'],weather['Temperature']['temp'],
-                                      weather['Temperature']['temp_max'],weather['Temperature']['temp_min'],
-                                      weather['Humidity'],weather['Pressure']['press'],
-				      weather['Wind']['speed'],weather['Wind']['deg'],))
+		weather_bot.weather_botCall(self,'Birmingham,uk')  
+	  
         elif msg['text']:
             self.sender.sendMessage(
 			    "I know naafing, code me: https://github.com/nasfarley88/west313-bot")
