69600db096476cb248d7936b6770b93edc2b66db
artemis-beta <KXZ186@student.bham.ac.uk>
2016-04-18 18:51:24 +0100

    Added Weather Bot with User Recognition

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import telepot
 import time
 from telepot.delegate import per_chat_id, create_open
+import weather_bot
+
+####################################################
 
 class WestieBot(telepot.helper.ChatHandler):
     def __init__(self, seed_tuple, timeout):
@@ -8,9 +11,20 @@ class WestieBot(telepot.helper.ChatHandler):
         super(WestieBot, self).__init__(seed_tuple, timeout)
 
     def on_message(self, msg):
-        """Simple on message thing."""
 
-        if msg['text']:
+        """Simple on message thing."""
+	
+	user = 'user'
+	for key in msg:
+		if key == 'from':
+			user = msg['from']['username']
+
+	#OpenWeather Implementation, check for 'weather' in input string
+	if 'weather' in msg['text'].lower():
+		weather_bot.weather_botCall(self,user,'Birmingham,uk')  
+		print "Weather Bot Accessed by %s" % user
+
+	elif msg['text']:
             self.sender.sendMessage(
 			    "I know naafing, code me: https://github.com/nasfarley88/west313-bot")
 
---
+++ weather_bot.py
@@ -0,0 +1,22 @@
+##########OPENWEATHER API & APPKEY##################
+import pyowm
+owm = pyowm.OWM(pickle.load(open(".weather_key.dat","rb"))
+####################################################
+
+def weather_botCall(self,user,location):
+	weatherLocationW313 = owm.weather_at_place(location)
+        w = weatherLocationW313.get_weather()
+        weather = {'Status': w.get_detailed_status(),
+                   'Time':w.get_reference_time(timeformat='iso'),
+                   'Humidity': w.get_humidity(),
+                   'Pressure': w.get_pressure(),
+                   'Temperature' : w.get_temperature(unit='celsius'),
+                   'Wind' : w.get_wind()}
+
+        self.sender.sendMessage(""" Hello %s! The weather for today at West 313 is currently: 
+                                    \nDate/Time: %s \nStatus: %s \nTemperature: %s C, High: %s C, Low: %s C 
+                                    \nHumidity: %s \nPressure: %s mb\nWind Speed: %s mph, %s Degrees"""
+                               % (user,weather['Time'],weather['Status'],weather['Temperature']['temp'],
+                                  weather['Temperature']['temp_max'],weather['Temperature']['temp_min'],
+                                  weather['Humidity'],weather['Pressure']['press'],
+                                  weather['Wind']['speed'],weather['Wind']['deg'],))
