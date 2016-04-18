cd832c0e61b62e15fd81c6c66afd3569380459ee
artemis-beta <KXZ186@student.bham.ac.uk>
2016-04-18 18:07:51 +0100

    Added User Recognition to Weather Bot

--- weather_bot.py
+++ weather_bot.py
@@ -3,7 +3,7 @@ import pyowm
 owm = pyowm.OWM('238da21e83fcfa9f499bbad8d9b365d0')
 ####################################################
 
-def weather_botCall(self,location):
+def weather_botCall(self,user,location):
 	weatherLocationW313 = owm.weather_at_place(location)
         w = weatherLocationW313.get_weather()
         weather = {'Status': w.get_detailed_status(),
@@ -13,10 +13,10 @@ def weather_botCall(self,location):
                    'Temperature' : w.get_temperature(unit='celsius'),
                    'Wind' : w.get_wind()}
 
-        self.sender.sendMessage(""" Weather at West 313 is currently: 
+        self.sender.sendMessage(""" Hello %s! The weather for today at West 313 is currently: 
                                     \nDate/Time: %s \nStatus: %s \nTemperature: %s C, High: %s C, Low: %s C 
                                     \nHumidity: %s \nPressure: %s mb\nWind Speed: %s mph, %s Degrees"""
-                               % (weather['Time'],weather['Status'],weather['Temperature']['temp'],
+                               % (user,weather['Time'],weather['Status'],weather['Temperature']['temp'],
                                   weather['Temperature']['temp_max'],weather['Temperature']['temp_min'],
                                   weather['Humidity'],weather['Pressure']['press'],
                                   weather['Wind']['speed'],weather['Wind']['deg'],))
