0b52afba7935f4142fe8d56d8d00d40601cf7d22
Nathanael Farley <nasfarley88@gmail.com>
2016-04-18 17:16:41 +0100

    Various minor fixes.

--- main.py
+++ main.py
@@ -11,31 +11,24 @@ class WestieBot(telepot.helper.ChatHandler):
         super(WestieBot, self).__init__(seed_tuple, timeout)
 
     def on_message(self, msg):
-
         """Simple on message thing."""
-	
-	user = 'user'
-	for key in msg:
-		if key == 'from':
-			user = msg['from']['username']
+        user = 'user'
+        for key in msg:
+            if key == 'from':
+                user = msg['from']['username']
 
-	#OpenWeather Implementation, check for 'weather' in input string
-	if 'weather' in msg['text'].lower():
-		weather_bot.weather_botCall(self,user,'Birmingham,uk')  
-		print "Weather Bot Accessed by %s" % user
-
-	elif msg['text']:
-            self.sender.sendMessage(
-			    "I know naafing, code me: https://github.com/nasfarley88/west313-bot")
+        #OpenWeather Implementation, check for 'weather' in input string
+        if 'weather' in msg['text'].lower():
+            weather_bot.weather_botCall(self, 'Birmingham,uk')
+            print("Weather Bot Accessed by %s" % user)
 
 def main():
     """Simple main."""
     import config
     bot = telepot.DelegatorBot(config.TG_KEY, [
         (per_chat_id(), create_open(WestieBot, timeout=10)),
-
     ])
-    bot.notifyOnMessage(run_forever=True)
+    bot.message_loop(run_forever=True)
 
 if __name__ == '__main__':
     main()
--- weather_bot.py
+++ weather_bot.py
@@ -1,22 +1,23 @@
 ##########OPENWEATHER API & APPKEY##################
 import pyowm
-owm = pyowm.OWM('238da21e83fcfa9f499bbad8d9b365d0')
+owm = pyowm.OWM('238da21e83fcfa9f499bbad8d9b365d0') # TODO remove key from hard code
 ####################################################
 
-def weather_botCall(self,location):
-	weatherLocationW313 = owm.weather_at_place(location)
-        w = weatherLocationW313.get_weather()
-        weather = {'Status': w.get_detailed_status(),
-                   'Time':w.get_reference_time(timeformat='iso'),
-                   'Humidity': w.get_humidity(),
-                   'Pressure': w.get_pressure(),
-                   'Temperature' : w.get_temperature(unit='celsius'),
-                   'Wind' : w.get_wind()}
+# This is not a *true* self, but it's as good a name as any
+def weather_botCall(self, location):
+    weatherLocationW313 = owm.weather_at_place(location)
+    w = weatherLocationW313.get_weather()
+    weather = {'Status': w.get_detailed_status(),
+                    'Time':w.get_reference_time(timeformat='iso'),
+                    'Humidity': w.get_humidity(),
+                    'Pressure': w.get_pressure(),
+                    'Temperature' : w.get_temperature(unit='celsius'),
+                    'Wind' : w.get_wind()}
 
-        self.sender.sendMessage(""" Weather at West 313 is currently: 
+    self.sender.sendMessage(""" Weather at West 313 is currently: 
                                     \nDate/Time: %s \nStatus: %s \nTemperature: %s C, High: %s C, Low: %s C 
                                     \nHumidity: %s \nPressure: %s mb\nWind Speed: %s mph, %s Degrees"""
-                               % (weather['Time'],weather['Status'],weather['Temperature']['temp'],
-                                  weather['Temperature']['temp_max'],weather['Temperature']['temp_min'],
-                                  weather['Humidity'],weather['Pressure']['press'],
-                                  weather['Wind']['speed'],weather['Wind']['deg'],))
+                            % (weather['Time'],weather['Status'],weather['Temperature']['temp'],
+                               weather['Temperature']['temp_max'],weather['Temperature']['temp_min'],
+                               weather['Humidity'],weather['Pressure']['press'],
+                               weather['Wind']['speed'],weather['Wind']['deg'],))
