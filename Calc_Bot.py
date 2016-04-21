64f59b920da147e58357f0910600534adea4c8c9
artemis-beta <KXZ186@student.bham.ac.uk>
2016-04-21 17:58:14 +0100

    Numpy Support for Calc Bot!

--- Calc_Bot.py
+++ Calc_Bot.py
@@ -1,11 +1,11 @@
 from math import *
+from numpy import *
 
 def Calc_Bot(self,msg):
 	input_calc = msg['text']
-	input_calc = input_calc.replace("/calc","",1)
-	print(input_calc)
+	input_calc = input_calc.replace("/calc ","",1)
 	output = eval(input_calc)
-	self.sender.sendMessage("%s = %s" % (input_calc,output))
+	self.sender.sendMessage("%s" % (output))
 
 __commands__ = {
 	"/calc" : Calc_Bot
