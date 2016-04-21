3558a48b89cd5c9d3b17746f71221a1b8d7bcfca
artemis-beta <KXZ186@student.bham.ac.uk>
2016-04-21 18:05:33 +0100

    Fixed Problem with Group Chat Calls

--- Calc_Bot.py
+++ Calc_Bot.py
@@ -3,7 +3,7 @@ from numpy import *
 
 def Calc_Bot(self,msg):
 	input_calc = msg['text']
-	input_calc = input_calc.replace("/calc ","",1)
+	input_calc = " ".join(filter(lambda x:x[0]!='/', input_calc.split()))
 	output = eval(input_calc)
 	self.sender.sendMessage("%s" % (output))
 
